dc12c86db59840c5ae6570afd3a8684d9a279ef3
Mickaël Floc’hlay <mickael@flochlay.com>
2018-03-19 20:07:04 +0100

    Bump to 0.7.1 + Fixed edge artefact when resizing picture with PreUploadProcessor

--- Example/Tests/PreUploadProcessorSpec.swift
+++ Example/Tests/PreUploadProcessorSpec.swift
@@ -12,7 +12,7 @@ class PreUploadProcessorSpec: QuickSpec {
                 let myResizedImage = preprocessor.resizeImage(myImage!, targetWidth: 1125.0)
                 expect(myResizedImage).toNot(beNil())
                 expect(myResizedImage?.size.width).to(equal(1125.0))
-                expect(myResizedImage?.size.height).to(equal(844.0))
+                expect(myResizedImage?.size.height).to(equal(843.0))
                 expect(UIImageJPEGRepresentation(myResizedImage!, 0.6)).toNot(beNil())
             }
 
